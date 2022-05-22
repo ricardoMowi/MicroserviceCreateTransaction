8098eb3f286a479ebc49ef9d192dab46fddf4944
Ricardo <sgroja6@gmail.com>
2022-05-22 05:07:19 -0500

    update repo

--- createTransaction/src/main/java/com/nttdata/createTransaction/CreateTransactionApplication.java
+++ createTransaction/src/main/java/com/nttdata/createTransaction/CreateTransactionApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.createTransaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CreateTransactionApplication {
 
 	public static void main(String[] args) {
